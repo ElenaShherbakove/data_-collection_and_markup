464aeed1b3283cfdf987213ab74a6ed89af71645
Elena <Shherbakove@yandex.ru>
2024-07-15 21:38:54 +0300

    Seminar4

---
+++ DZ/DZ_4.py
@@ -0,0 +1,18 @@
+"""Сайты для парсинга таблиц.
+
+https://www.worldometers.info/
+- 
+imdb
+https://finance.yahoo.com/trending-tickers/
+https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)
+https://proxyway.com/guides/best-websites-to-practice-your-web-scraping-skills
+
+
+Интересные апи
+https://kinopoisk.dev/
+https://openweathermap.org/api
+
+
+
+Датасеты в неограниченном количестве
+https://www.kaggle.com/"""
---
+++ Seminar4/app.py
@@ -0,0 +1,4 @@
+from lxml import etree
+
+tree = etree.parse("Seminar4/web_page.html")
+print(tree)
